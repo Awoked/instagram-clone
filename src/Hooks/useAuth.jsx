44fa8ddbbec3d453c56448321da53651ce967f18
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-08-16 13:04:55 +0300

    update

---
+++ src/Hooks/useAuth.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect } from 'react'
+import { UserContext } from '../Context/UserContext'
+
+/**
+ * @typedef {{
+ * userName: string,
+ * password: string,
+ * firstName: string,
+ * lastName: string,
+ * email: string,
+ * password: string;
+ * }} userType
+ */
+
+const useUser = () => {
+    const { userData, setUserData } = useContext(UserContext);
+
+    useEffect(() => {
+        console.log(userData);
+    }, [userData])
+
+
+
+    const Login = async (data) => {
+        // api işlemi
+        setUserData(data)
+        if (false) {
+            //Hata
+            throw new Error("Login failed")
+        }
+    }
+
+    return {
+        userData,
+        Login,
+    }
+}
+
+export default useUser
